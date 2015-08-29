a3868727668c83a3ebc5cd0e5c44d51c1d1bcd82
Matteo Gaggiano <maxxxx92@gmail.com>
2015-08-29 15:10:35 +0200

    Added support for MacOSX compilation and clean

--- Linux/psxtract.c
+++ Linux/psxtract.c
@@ -932,7 +932,7 @@ int main(int argc, char **argv)
 	printf("Unpacking PBP %s...\n", input_filename);
 
 	// Setup a new directory to output the unpacked contents.
-	#ifdef __linux__
+	#if defined(__linux__) || defined(__APPLE__)
 		mkdir("PBP", 0777);
 	#else
 		mkdir("PBP");
@@ -956,7 +956,7 @@ int main(int argc, char **argv)
 	// Make a directory for CD-ROM images if required.
 	if (conv)
 	{
-		#ifdef __linux__
+		#if defined(__linux__) || defined(__APPLE__)
 			mkdir("CDROM", 0777);
 		#else
 			mkdir("CDROM");
@@ -964,7 +964,7 @@ int main(int argc, char **argv)
 	}
 
 	// Make a new directory for the ISO data.
-	#ifdef __linux__
+	#if defined(__linux__) || defined(__APPLE__)
 		mkdir("ISO", 0777);
 	#else
 		mkdir("ISO");
